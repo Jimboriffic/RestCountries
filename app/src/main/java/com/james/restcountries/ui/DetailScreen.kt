454c604a9c17eb4b732f4f1426e24c71404ebf76
Jimboriffic <james.sliwa@gmail.com>
2026-03-06 15:51:55 +0100

    Added comments to DetailScreen

--- app/src/main/java/com/james/restcountries/ui/DetailScreen.kt
+++ app/src/main/java/com/james/restcountries/ui/DetailScreen.kt
@@ -18,34 +18,37 @@ import com.james.restcountries.util.NumberFormatter
 @Composable
 fun DetailScreen(
     countryNameEncoded: String?,
-    viewModel: CountryViewModel = viewModel() // ✅ default
+    viewModel: CountryViewModel = viewModel() // default
 ) {
     val decodedName = countryNameEncoded?.let {
         URLDecoder.decode(it, StandardCharsets.UTF_8.toString())
     }
 
+    // Look up selected country from the ViewModel's cached list using the decoded common name
     val country = viewModel.countries.value.find { it.name.common == decodedName }
 
     if (country == null) {
         Text("Loading information")
         return
     }
-
+    // Vertical layout for the country details screen
     Column(modifier = Modifier.padding
         (16.dp,
         end = 16.dp,
         bottom = 16.dp,
         top = 80.dp))
-    {
+    { // Load and display the country's flag from the api
         AsyncImage(
             model = country.flags?.png,
             contentDescription = "Flag of ${country.name.common}",
             contentScale = ContentScale.Fit
         )
 
+        // Text fields for displaying specific country information
         Text("Name: ${country.name.common}")
         Text("Capital: ${country.capital?.joinToString() ?: "N/A"}")
         Text("Region: ${country.region ?: "N/A"}")
+        // Here numberFormatter is used for better readability
         Text("Population: ${country.population?.let(NumberFormatter::populationDotGrouping) ?: "N/A"}")
 
     }
